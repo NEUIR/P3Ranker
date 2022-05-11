e45506622bec50851150daa3152d9ad50390863d
Xiaomeng Hu <89115107+GregxmHu@users.noreply.github.com>
2022-05-11 10:18:17 +0800

    Update __init__.py

--- src/data/datasets/__init__.py
+++ src/data/datasets/__init__.py
@@ -1,5 +1,4 @@
 from src.data.datasets.dataset import Dataset
-from src.data.datasets.edrm_dataset import EDRMDataset
 from src.data.datasets.bert_dataset import BertDataset
 from src.data.datasets.roberta_dataset import RobertaDataset
 from src.data.datasets.bertmlm_dataset import BertMLMDataset
